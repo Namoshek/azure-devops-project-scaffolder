ea7c2e0bbf412a78864fc95ae710ad9bbf755b06
Marvin Mall <marvin-mall@msn.com>
2026-03-24 18:30:02 +0100

    Add ado-devops-ui styling

--- src/Hub/index.tsx
+++ src/Hub/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as SDK from "azure-devops-extension-sdk";
 import { ScaffoldApp } from "./ScaffoldApp";
+import "azure-devops-ui/Core/override.css";
 
 SDK.init().then(() => {
   ReactDOM.render(<ScaffoldApp />, document.getElementById("root"));
